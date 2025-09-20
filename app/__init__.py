44ed25f6f77a701dbf8f7b1a47f7744c3b520fe7
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2025-09-20 09:36:49 -0500

    add: 404 page. update: gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+
 from .blueprints import register_blueprints
-from .extensions import db, migrate
 from .config import Config
+from .extensions import db, migrate
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
 
     # == CONFIGURACIÓN ==
     app.config.from_object(config_class)
-    
+
     # == INICIALIZAR EXTENSIONES ==
     db.init_app(app)
     migrate.init_app(app, db)
 
     # == IMPORTAR MODELOS ==
-    from . import models # noqa: F401-
-    
+    from . import models  # noqa: F401
+
     # == REGISTRAR BLUEPRINTS ==
     register_blueprints(app)
+
+    # == 404
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("errors/404.html"), 404
 
     return app
